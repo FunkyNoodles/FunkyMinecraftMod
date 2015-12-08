54a6e824ce928ada26234ceba2ce485220c66834
Funky Noodles <nickasds@hotmail.com>
2015-12-08 17:19:50 -0600

    Thermometer now stays up all the time

--- src/main/java/io/github/funkynoodles/ThermoGui.java
+++ src/main/java/io/github/funkynoodles/ThermoGui.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ThermoGui extends GuiIngame{
@@ -20,19 +21,24 @@ public class ThermoGui extends GuiIngame{
 		// TODO Auto-generated constructor stub
 		this.mc = mcIn;
 	}
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public void renderThermoGui(RenderGameOverlayEvent event){
 		ScaledResolution scaledresolution = new ScaledResolution(this.mc, this.mc.displayWidth, this.mc.displayHeight);
 		//this.mc.renderEngine.bindTexture(funky_icons);
-		if (event.isCancelable() || event.type != ElementType.EXPERIENCE) {
+		//this.mc.getTextureManager().bindTexture(funky_icons);
+		//this.drawTexturedModalRect(scaledresolution.getScaledWidth()-25, scaledresolution.getScaledHeight()-58, 21, 0, 20, 54);
+
+		if (event.isCancelable() || event.type != ElementType.HEALTH) {
 			return;
 		}
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		//this.mc.renderEngine.bindTexture(funky_icons);
-		this.mc.getTextureManager().bindTexture(funky_icons);
+		this.mc.renderEngine.bindTexture(funky_icons);
 		this.drawTexturedModalRect(scaledresolution.getScaledWidth()-25, scaledresolution.getScaledHeight()-58, 21, 0, 20, 54);
 		//this.mc.getTextureManager().bindTexture(funky_icons);
+		this.mc.renderEngine.bindTexture(icons);
+
 
 	}
 }
